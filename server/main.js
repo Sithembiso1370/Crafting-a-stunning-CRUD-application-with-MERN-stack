fec9ce67010a9e89c80bb42db26ef9f41c8538bb
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2022-10-25 12:42:42 +0200

    Project complete frontend and backend working to add Todos

--- server/main.js
+++ server/main.js
@@ -5,6 +5,7 @@ const express = require("express");
 const cors = require("cors");
 
 const databaseConfiguration = require('./configurations/database.js');
+const todo = require("./routes/todo.routes.js");
 
 const dotenv = require("dotenv");
 
@@ -18,10 +19,16 @@ const PORT = process.env.PORT || 5000;
 //connecting to the mongodb database
 databaseConfiguration();
 
+//adding cors
+app.use(cors({ origin: true, credentials: true }));
+
 // add the middlewares
 app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('<h1>Server is up and running</h1>'));
 
+
+// using our routes
+app.use("/api/todoapp", todo);
 // listen
 // Using the app, create a get method for the endpoint http://localhost:4000.
 app.listen(PORT, () =>
